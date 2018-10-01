2a1e91a3cfc3658581e82f8b893be8724f31a7ba
Enrique de Jesus Talavera Ordaz <etalavera_l@D7627S054E039.bradesco.com.mx>
2018-10-01 17:27:05 -0500

    testing

--- api/config/app.php
+++ api/config/app.php
@@ -160,7 +160,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Barryvdh\Cors\ServiceProvider::class
+        Barryvdh\Cors\ServiceProvider::class,
+        // JWT Auth
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        Irazasyed\JwtAuthGuard\JwtAuthGuardServiceProvider::class,
 
     ],
 
@@ -210,7 +213,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class, 
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
     ],
 
 ];
